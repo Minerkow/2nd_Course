c34e1cfb0aa2bcf5836a4cd87304309570752132
Minerkow <melnikov.ignat@phystech.edu>
2020-10-21 18:31:01 +0300

    edit cmake and add main.c

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "Matrix.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    size_t n;
+    std::cin >> n;
+    mtrx::Matrix_t<double> matrix{n, n};
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            double input = 0.0;
+            std::cin >> input;
+            matrix[i][j] = input;
+        }
+    }
+    std::cout << matrix.Determinant();
     return 0;
 }
