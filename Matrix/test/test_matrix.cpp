c1e9270db20dc832104436634a8145e7421182fc
Minerkow <melnikov.ignat@phystech.edu>
2020-11-08 19:16:51 +0300

    add tests

--- Matrix/test/test_matrix.cpp
+++ Matrix/test/test_matrix.cpp
@@ -1,4 +1,5 @@
 #include "../Matrix.hpp"
+#include "test_generator.hpp"
 #include <gtest/gtest.h>
 
 int main() {
@@ -62,6 +63,7 @@ TEST(Matrix, Matrix_Mult) {
     ASSERT_EQ(res, trueRes);
 }
 
+
 TEST(Matrix, Operators) {
     mtrx::Matrix_t<double>  matrix1 {{ {1.1, 1.1, 1.1},
                                        {1.1, 1.1, 1.1},
@@ -88,26 +90,43 @@ TEST(Matrix, Operator_SqBrace) {
     ASSERT_NEAR(1.1, matrix[1][1], mtrx::PRESISION);
 }
 
-TEST(Matrix, Determinant) {
+TEST(Matrix, Constructor) {
+    mtrx::Matrix_t<int> matrix{{{1, 2, 3, 4},
+                                {5, 6, 7, 8},
+                                {9, 10, 11, 12}}};
+    mtrx::Matrix_t<double> matrix1{matrix};
+    mtrx::Matrix_t<double> res{{{1.0, 2.0, 3.0, 4.0},
+                                {5.0, 6.0, 7.0, 8.0},
+                                {9.0, 10.0, 11.0, 12.0}}};
+    ASSERT_EQ(matrix1, res);
+}
+
+TEST(Matrix, Determinant1) {
     mtrx::Matrix_t<int> matrix{{{1, 2, 3, 4, 5},
-                                {6, 7, 8, 9, 10},
-                                {11, 12, 13, 14, 15},
-                                {16, 17, 18, 19, 20},
-                                {21, 22, 23, 24, 25}}};
+                                       {6, 7, 8, 9, 10},
+                                       {11, 12, 13, 14, 15},
+                                       {16, 17, 18, 19, 20},
+                                       {21, 22, 23, 24, 25}}};
 
     ASSERT_NEAR(matrix.Determinant(), 0, mtrx::PRESISION);
+}
 
+TEST(Matrix, Determinant2) {
     mtrx::Matrix_t<int> matrix1{{{1, 2},
                                  {3, 4}}};
 
     ASSERT_NEAR(matrix1.Determinant(), -2, mtrx::PRESISION);
+}
 
-    mtrx::Matrix_t<int> matrix2{{{1, 2, 3},
-                                 {4, -2, 6},
-                                 {7, 8, 9}}};
+TEST(Matrix, Determinant3) {
+        mtrx::Matrix_t<int> matrix2{{{1, 2, 3},
+                                     {4, -2, 6},
+                                     {7, 8, 9}}};
 
-    ASSERT_NEAR(matrix2.Determinant(), 84, mtrx::PRESISION);
+        ASSERT_NEAR(matrix2.Determinant(), 84, mtrx::PRESISION);
+}
 
+TEST(Matrix, Determinant4) {
     mtrx::Matrix_t<double> matrix3{{{1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1},
                                     {1.1, 2, 1.1, 1.1, 1.1, 1.1, 1.1},
                                     {1.1, 1.1, 3, 1.1, 1.1, 1.1, 1.1},
@@ -118,3 +137,38 @@ TEST(Matrix, Determinant) {
 
     ASSERT_NEAR(matrix3.Determinant(), 398.46064, mtrx::PRESISION);
 }
+
+TEST(Matrix, Determinant5) {
+    mtrx::Matrix_t<int> matrix{{{0, 0, 0, -1},
+                                {0, 0, -1, 0},
+                                {0, 1, 0, 0},
+                                {1, 0, 0, 0}}};
+    ASSERT_NEAR(matrix.Determinant(), 1, mtrx::PRESISION);
+}
+
+TEST(Matrix, Determinant6) {
+    mtrx::Matrix_t<int> matrix{{{84, 75, 42, 19, 1},
+                                {52, 101, 78, 70, 7},
+                                {18, 68, 49, 36, 3},
+                                {17, 61, 59, 73, 8},
+                                {2, 7, 7, 9, 1}}};
+    ASSERT_NEAR(matrix.Determinant(), 1, mtrx::PRESISION);
+}
+
+TEST(Matrix, Determinant7) {
+    mtrx::Matrix_t<int> matrix{{{0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0}}};
+    ASSERT_NEAR(matrix.Determinant(), 0, mtrx::PRESISION);
+}
+
+//TEST(Matrix, Mult100x100) {
+//    matrixgen::MatrixGenerator_t gen{100};
+//    std::cout << gen << std::endl;
+//
+//    std::cout << gen.determinant_ << std::endl;
+//
+//    std::cout << gen.matrix_.Determinant();
+//}
