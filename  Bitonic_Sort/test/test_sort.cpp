82139339d63785d5ffd8fb7d4862d50b95b9e50e
Minerkow <melnikov.ignat@phystech.edu>
2020-12-06 13:31:23 +0300

    rm

---  Bitonic_Sort/test/test_sort.cpp
+++
@@ -1,7 +0,0 @@
-#include "../Bitonic_Sort.h"
-#include <gtest/gtest.h>
-
-int main() {
-    testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
-}
--- 2Q/tests.cpp
+++
@@ -1,9 +0,0 @@
-#include "2Q.hpp"
-#include "TestGenerator.h"
-
-enum {NUM_TESTS = 10};
-
-int main() {
-    TestRandom(NUM_TESTS);
-    return 0;
-}
